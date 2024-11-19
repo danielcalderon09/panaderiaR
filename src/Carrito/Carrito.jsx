92e25db3fa9e19ce0131aa20f724b72f27cfb4e3
Daniel Felipe Calderon <0620danielquintero@gmail.com>
2024-11-19 09:56:54 -0500

    ajuste estilo informacion

---
+++ src/Carrito/Carrito.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ListaCarrito from './ListaCarrito';
+
+const Carrito = () => {
+    return (
+        <div className="carrito">
+        <ListaCarrito />
+      </div>
+    );
+  };
+  
+  export default Carrito;
